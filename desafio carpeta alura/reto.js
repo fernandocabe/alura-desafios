7501b8c137e5eba706b134bc114d1324ac06c893
fernandocabe <fernandocabezazsan@gmail.com>
2025-02-05 23:30:23 -0500

    Add files via upload

---
+++ desafio carpeta alura/reto.js
@@ -0,0 +1,18 @@
+alert('¡Bienvenida y bienvenido a nuestro sitio web!');  // le da la bienvenida
+let nombre = 'Lua';                                                              // variable determinada
+let eda = 25;                                                                    // variable de edad
+let numeroDeVentas = 50;                                              
+let saldoDisponible = 1000;  
+
+alert('¡Error!, Completa todos lo campos');   
+
+let mensajeDeError = '¡Error!, Completa todos lo campos';   
+alert(mensajeDeError);    
+
+nombre = prompt('digite tu Nombre  ');                // 8
+let edad = prompt('¿Cuantos años tienes: ?');          // 9
+   if (edad >= 18 ){
+    alert('¡Puedes obtener tu licencia de conducir!')
+   }else{
+    alert('No tienes la edad suficiente')
+   };       
